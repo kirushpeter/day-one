d435446a801cc69bcb4b5c3bf9df869bf0ddbc74
pip-peter <peterkir5693@outlook.com>
2020-01-14 20:42:37 +0300

    second commit

---
+++ day one.py
@@ -0,0 +1,18 @@
+print ("hello world")
+
+print("The capaybala likes to swim very fast")
+#prints as its written
+print ("""i am a new
+ trick""")
+name = input("what is your name? ")
+print(name)
+
+firstName = input("what is your first name?")
+lastName = input("what is your lastname?")
+print("hello " + firstName + " " + lastName)
+
+message = "Hello world"
+
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
