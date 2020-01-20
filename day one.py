ddbb201f0d6834d55b578053d301f24773ccf2a8
pip-peter <peterkir5693@outlook.com>
2020-01-20 18:52:51 +0300

    new commit

--- day one.py
+++ day one.py
@@ -1,10 +1,15 @@
-print ("hello world")
+'''print ("hello world")
 
 print("The capaybala likes to swim very fast")
 #prints as its written
 print ("""i am a new
  trick""")
+
 name = input("what is your name? ")
+country = input("what country do you live in? ")
+country = country.upper()
+print()
+
 print(name)
 
 firstName = input("what is your first name?")
@@ -15,4 +20,60 @@ message = "Hello world"
 
 print(message.lower())
 print(message.upper())
-print(message.swapcase())
+#message = "Hello world"
+
+#print(message.find("world"))
+#print(message.count("o"))
+#print(message.capitalize())
+#print(message.replace("Hello","Hi"))
+# 
+'''
+"""
+firstname = input("what is your firstname? ")
+lastname = input("what is your lastname?")
+print(firstname.upper())
+print(lastname.upper() """
+
+
+"""area = 0
+height = 10
+width = 20 
+#calaculates the area of a triangle.
+
+area = width * height / 2
+
+print("the area of the triangle would be %f" % area)"""
+
+"""
+salary = input("please enter your salary: ")
+
+bonus = input("please enter your bonus: ")
+
+payCheck = int(salary) + float(bonus)
+
+print(payCheck)"""
+
+
+"""
+import datetime
+
+currentDate = datetime.date.today()
+
+print(currentDate)
+
+print(currentDate.year)
+
+print(currentDate.month)
+
+print(currentDate.day)
+
+print(currentDate.strftime("%d-%b-%Y"))
+"""
+
+
+answer = input("would you like express shipping?")
+
+if answer == "yes":
+    print("that would be an extra $19")
+
+    
